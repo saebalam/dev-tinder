08d8ce9b07f2973530a40d9994a02d6c4caa0769
saebalam <saebalam123@gmail.com>
2024-12-29 12:59:38 +0530

    psd

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-  await mongoose.connect("mongodb+srv://saebalam:saebalampassword@cluster0.yms88.mongodb.net/devTinder");
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDb;
